3f46b7df374b34627cc97e811f76fd1900cc7cc2
unknown <602165057@qq.com>
2015-07-15 15:25:00 +0800

    项目基本配置，log4j 数据库配置

---
+++ conf/db.js
@@ -0,0 +1,11 @@
+// conf/db.js
+// MySQL数据库联接配置
+module.exports = {
+	mysql: {
+		host: 'localhost', 
+		user: 'root',
+		password: 'root',
+		database:'nodejs', // 前面建的user表位于这个数据库中
+		port: 3306
+	}
+};
